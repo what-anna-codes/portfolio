29358f2fab4a77df43b4f94e8e907702dd655e3a
what-anna-codes <annakarpiuk@zohomail.eu>
2026-07-22 01:02:02 +0200

    feat: add classNames param

--- app/components/ui/SectionHeader/SectionHeader.tsx
+++ app/components/ui/SectionHeader/SectionHeader.tsx
@@ -3,13 +3,15 @@ import { ReactNode } from "react";
 export const SectionHeader = ({
   content,
   color,
+  classNames
 }: {
   content: string | ReactNode;
   color?: string;
+  classNames?: string
 }) => {
   return (
     <h2
-      className={`${color || "text-mauve-600"} p-12 pb-8 text-5xl font-semibold leading-10 tracking-tight`}>
+      className={`${color || "text-mauve-600"} p-12 pb-8 text-5xl font-semibold leading-10 tracking-tight ${classNames}`}>
       {content}
     </h2>
   );
