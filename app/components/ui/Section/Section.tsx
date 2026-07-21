844f8f6d3e813f226a0e32509435f7b61efb020c
what-anna-codes <annakarpiuk@zohomail.eu>
2026-07-22 00:59:54 +0200

    feat: add id & classNames params

--- app/components/ui/Section/Section.tsx
+++ app/components/ui/Section/Section.tsx
@@ -1,15 +1,21 @@
 import { ReactNode } from "react";
 
 export const Section = ({
+  id,
+  classNames,
   children,
   bg,
 }: {
+  id?: string;
+  classNames?: string;
   children: ReactNode;
   bg?: string;
 }) => {
   return (
     <div
-      className={`${bg || "bg-mauve-50"} pb-8 max-w-3xl border-t-2 border-b-2 border-mauve-300`}>
+      id={id}
+      className={`${bg || "bg-mauve-50"} flex flex-col flex-1 pb-12 pt-12 w-3xl
+      border-t-2 border-b-2 border-mauve-300 ${classNames}`}>
       {children}
     </div>
   );
