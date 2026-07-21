9bbb00481470b7708b972c7be01b8f175e88b69f
what-anna-codes <annakarpiuk@zohomail.eu>
2026-07-22 00:51:16 +0200

    refactor: polish text content

--- app/data/about.tsx
+++ app/data/about.tsx
@@ -1,42 +1,72 @@
 export const about_pl = {
   title: "więcej o mnie",
   intro: `
-    <p className="w-[80%] text-pretty pl-0">Nim na poważnie zainteresowałam się programowaniem, moją główną pasją było <strong>poznawanie obcych języków</strong>.
-      Przypadek? Nie sądzę ^^. Obie dziedziny premiują podobne cechy: cierpliwość, upór w tropieniu błędów, szukanie schematów, integrowanie nowych okruchów wiedzy w logiczne systemy, których używanie szybko "wchodzi w krew"... Ale wróćmy do tematu.
+    <p>
+      Nim na poważnie zainteresowałam się programowaniem, moją główną pasją
+      było <strong>poznawanie obcych języków</strong>. Przypadek? Nie sądzę ^^.
+      Obie dziedziny premiują podobne cechy: cierpliwość, upór w tropieniu błędów,
+      integrowanie nowych okruchów wiedzy w logiczne systemy,
+      których używanie szybko "wchodzi w krew"... Ale wróćmy do tematu.
     </p>
     <br />
     <p>
       Jako dyplomowana lingwistka wyspecjalizowałam się w
-      tłumaczeniach pisemnych <strong> na język angielski.</strong> Brałam na warsztat teksty <strong> naukowe, urzędowe, prawnicze, marketingowe, edukacyjne, prasowe... </strong>
-      Po drugim dyplomie - z filologii japońskiej - nawiązałam też współpracę z trzema wydawnictwami publikującymi autorów azjatyckich. To zaś zaowocowało przeróżnymi <strong>przekładami literackimi z języka japońskiego</strong>, wydanymi na polskim rynku:
+      tłumaczeniach pisemnych <strong> na język angielski.</strong> Brałam na warsztat
+      teksty <strong> naukowe, urzędowe, prawnicze, marketingowe, edukacyjne,
+      prasowe... </strong> Po drugim dyplomie - z filologii japońskiej -
+      nawiązałam też współpracę z trzema wydawnictwami publikującymi autorów azjatyckich.
+      To zaś zaowocowało przeróżnymi <strong>przekładami literackimi
+      z języka japońskiego</strong>, wydanymi na polskim rynku:
     </p>
   `,
-outro: `
+  outro: `
     <p>
-      Znalazł się czas na literaturę piękną, dziecięcą i komiks (w sumie ponad 50 tomów). Ale Azja to dla mnie też miejsce, w którym przeżyłam unikalne przygody - <strong>rok studiów w Kioto, pół roku pracy w chińskim gimnazjum, miesięczne workation w Wietnamie</strong>. I choć od czterech lat całą energię wkładam w zgłębianie zawiłości programowania, tematy dalekowschodnie wciąż są mi bliskie. Czy włączam japońskie podcasty, czy podciągam się z podstaw chińskiego, wszędzie tam odnajduję szczególną harmonię, spokojny rytm, w którym milej jest żyć.
+      Znalazł się czas na literaturę piękną, dziecięcą i komiks (w sumie ponad 50 tomów).
+      Ale Azja to dla mnie też miejsce, w którym przeżyłam jedyne w swoim rodzaju przygody,
+      takie jak <strong>rok studiów w Kioto, pół roku pracy w syczuańskim gimnazjum,
+      czy miesięczne workation w Wietnamie</strong>. I choć od czterech lat całą energię
+      wkładam w zgłębianie zawiłości programowania, tematy dalekowschodnie wciąż są mi bliskie.
+      Czy włączam japońskie podcasty, czy podszkalam się z podstaw chińskiego, wszędzie tam
+      odnajduję szczególną harmonię, spokojny rytm, w którym milej jest żyć.
     </p>
   `,
 };
 
 export const about_en = {
-  title: 'more about me',
+  title: "more about me",
   intro: `
     <p>
       Before I plunged into the world of coding,
-      my main passion was learning foreign languages.
-      Coincidence? I think not ^^.
-      Both fields reward similar traits:
+      my main passion was <strong>learning foreign languages</strong>.
+      Coincidence? I think not ^^. Both fields reward similar traits:
       patience, persistence in tracking down errors,
-      habituary pattern-seeking, and integrating new bits of knowledge into logical systems that quickly become second nature...
-      But back to the topic.
+      habitual pattern-seeking, and integrating new bits of knowledge
+      into logical systems that quickly become second nature...
+      But let's get back to the topic.
     </p>
+    <br />
     <p>
-      As a certified linguist, I specialized in written translations into English. I worked on academic, official, legal, marketing, educational, and press texts... After my second degree—in Japanese studies—I also started collaborating with three publishing houses that release Asian authors. This resulted in various literary translations from Japanese published on the Polish market:
+      As a certified linguist, I specialized in translations into English.
+      My portfolio included <strong> academic papers, legal documents,
+      marketing & educational materials, press releases...</strong> you name it.
+      After my second degree—in Japanese studies—I also started collaborating
+      with three publishing houses that release Asian authors.
+      This resulted in successful <strong>literary translations from Japanese</strong>
+      published on the Polish market:
     </p>
   `,
   outro: `
     <p>
-      The list is quite eclectic, including fine literature, children's books, and comics (over 50 volumes in total!). But for me, Asia is also a place where I experienced unique adventures—a year of studying in Kyoto, six months of working at a Chinese middle school, and a month-long workation in Vietnam. And although I've been putting all my energy into exploring the intricacies of programming for the past four years, Far Eastern topics are still close to my heart. Whether I'm tuning into Japanese podcasts or brushing up on my basic Chinese, I find a special harmony everywhere there—a peaceful rhythm that makes life more enjoyable.
+      The list is quite eclectic, including fine literature, children's books,
+      and comics (over 50 volumes in total!). But for me, Asia is also
+      the place of extraordinary adventures such as <strong> the year
+      I spent studying at Doshisha University in Kyoto, six months working
+      at a Chinese middle school, and a month-long workation in Vietnam</strong>.
+      And although I've been putting all my energy into exploring
+      the intricacies of programming for the past four years,
+      Far Eastern topics are still close to my heart. Whether I'm tuning into
+      Japanese podcasts or brushing up on my basic Chinese, I find
+      a special harmony everywhere there—a peaceful rhythm that makes life more enjoyable.
     </p>
   `,
 };
