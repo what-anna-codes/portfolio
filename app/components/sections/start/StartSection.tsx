979acd5c75353886c8ce554f3810e1934f1b2732
what-anna-codes <annakarpiuk@zohomail.eu>
2026-07-22 01:01:23 +0200

    refactor: adjust to new params in ui components

--- app/components/sections/start/StartSection.tsx
+++ app/components/sections/start/StartSection.tsx
@@ -11,26 +11,27 @@ export const StartSection = () => {
   const [isHovered, setHovered] = useState(false);
   const isEnglish = language === "en";
   return (
-    <Section>
+    <Section id="start">
       <div
         style={{
           transition: "all 1s",
           filter: !isHovered
-            ? "  contrast(150%) grayscale(100%)"
+            ? "contrast(150%) grayscale(100%)"
             : "contrast(110%) grayscale(0%)",
         }}>
         <SectionHeader content="anna karpiuk" />
         <div
-          className="flex  h-60"
+          className="flex h-60"
           onMouseEnter={() => setHovered(true)}
           onMouseLeave={() => setHovered(false)}>
           <TextBlock
+            classNames="w-[80%]"
             content={isEnglish ? start_en.description : start_pl.description}
           />
           <div className="relative h-full w-[50%]">
             <Image
               style={{ transition: "all 1s" }}
-              className="absolute z-2 drop-shadow-2xl bottom-0 right-0 max-h-fit h-auto  object-bottom-right drop-shadow-mauve-900 object-contain"
+              className="absolute z-2 drop-shadow-2xl -bottom-12 right-0 h-auto object-bottom-right drop-shadow-mauve-900 object-contain"
               src={`/img/profile/anna-color.png`}
               alt="anna outline"
               width={480}
