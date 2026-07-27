0a822301a8e6a46d5bb030a03025100335cb2a22
what-anna-codes <annakarpiuk@zohomail.eu>
2026-07-27 19:39:47 +0200

    chore: add book ids

--- app/data/books.tsx
+++ app/data/books.tsx
@@ -1,5 +1,6 @@
 export const books = [
   {
+    id: "book-1",
     filename: "podziemie-pamięci.jpg",
     title: "Podziemie pamięci",
     author: "Yōko Ogawa",
@@ -7,12 +8,14 @@ export const books = [
       "Jeśli nie na­uczy­my się za­peł­niać próż­ni po tym, co znik­nę­ło, wkrót­ce obu­dzi­my się w kra­inie sa­mych wyrw.",
   },
   {
+    id: "book-2",
     filename: "grobowa-cisza.jpg",
     title: "Grobowa cisza, żałobny zgiełk",
     author: "Yōko Ogawa",
     quote: `Czasem w życiu wszystko psuje się przez jeden płaszcz albo jedną kroplę do oka.`,
   },
   {
+    id: "book-3",
     filename: "cytryna.jpg",
     title: "Cytryna",
     author: "Motojirō Kajii",
@@ -20,63 +23,75 @@ export const books = [
       "Potrzebuję tragedii. Tylko tragedia pozwala złapać mi równowagę, a mętnym rojeniom umysłu nadaje ostrość wyrazu. Moja dusza łaknie melancholii niczym zły demon. Dopiero do cna przesiąknięta melancholią może osiągnąć ukojenie.",
   },
   {
+    id: "book-4",
     filename: "biała-księżniczka.jpg",
     title: "Opowieści o białej księżniczce",
     author: "Clamp",
   },
   {
+    id: "book-5",
     filename: "skamieniałe-sny.jpg",
     title: "Skamieniałe sny",
     author: "Satoshi Kon",
   },
   {
+    id: "book-6",
     filename: "psycho-pass.jpg",
     title: "Psycho-Pass 2",
     author: "Saru Hashino, Gen Urobuchi",
   },
   {
+    id: "book-7",
     filename: "inspektor-akane-tsunemori.jpg",
     title: "Inspektor Akane Tsunemori",
     author: "Gen Urobuchi",
   },
   {
+    id: "book-8",
     filename: "pierwsza-pomoc.jpg",
     title: "Pierwsza pomoc",
     author: "Gen-ichiro Yagyu",
   },
   {
+    id: "book-9",
     filename: "blue-phobia.jpg",
     title: "Blue Phobia",
     author: "Eri Tsuruyoshi",
   },
   {
+    id: "book-10",
     filename: "pan-cykada.jpg",
     title: "Pan Cykada",
     author: "Ryuko Iwabuchi",
   },
   {
+    id: "book-11",
     filename: "hal.webp",
     title: "Hal",
     author: "Ayase Umi",
   },
   {
+    id: "book-12",
     filename: "relife.jpg",
     title: "ReLife",
     author: "YayoiSo",
   },
-
   {
+    id: "book-13",
     filename: "siedmiu-książąt.jpg",
     title: "Siedmiu książąt i tysiącletni labirynt",
     author: " Ryuko Iwabuchi",
   },
   {
+    id: "book-14",
     filename: "gwiazda-spadająca-za-dnia.jpg",
     title: "Gwiazda spadająca za dnia",
     author: "Yamamori Mika",
   },
-  { filename: "gangsta.jpg",
+  {
+    id: "book-15",
+    filename: "gangsta.jpg",
     title: "Gangsta",
-    author: "Kohske"
+    author: "Kohske",
   },
 ];
