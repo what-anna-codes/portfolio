773f855f0a3ec0c7cfbbc2553ae90f0659179037
what-anna-codes <annakarpiuk@zohomail.eu>
2026-07-29 02:59:48 +0200

    chore: reorder social media icons

--- app/components/sections/start/LinkPanel.tsx
+++ app/components/sections/start/LinkPanel.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { contact } from "@/app/data/contact";
 
 export const LinkPanel = () => {
-  const media = ["github", "linkedin"] as const;
+  const media = ["linkedin", "github"] as const;
   return (
     <div className="mt-8 ml-12 max-sm:place-self-center w-fit max-sm:ml-0 flex justify-center gap-4 h-10">
       {Array.isArray(media) &&
