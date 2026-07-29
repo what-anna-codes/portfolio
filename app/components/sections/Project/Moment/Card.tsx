97393c877d8c4bcc9b87f2b30a610424f1c7aa4a
what-anna-codes <annakarpiuk@zohomail.eu>
2026-07-29 03:01:24 +0200

    feat: add cards for the project stages component

---
+++ app/components/sections/Project/Moment/Card.tsx
@@ -0,0 +1,27 @@
+import { TextBlock } from "@/app/components/ui/TextBlock/TextBlock";
+
+export const Card = ({ data }: any) => {
+  const { position, title, lead, picture } = data;
+
+  return (
+    <div className="ml-2 w-full relative  h-fit mt-12">
+      <div
+        style={{ transition: "all 0.8s" }}
+        className="rounded-full absolute z-10 left-0  brightness-75 hover:brightness-100 bg-stone-400 border-white   border outline-3  shadow-md hover:shadow-xl overflow-hidden w-25 md:w-30 h-auto aspect-square     outline-stone-200 hover:text-stone-600/90">
+        <img
+          src={`${process.env.PUBLIC_URL ?? ""}/img/icons/numbers/number-${position}.png`}
+          alt={picture}
+          className=" h-full aspect-video w-auto object-cover object-center scale-110 duration-1000"
+        />
+      </div>
+
+      <h3 className="h-13  border-b-6 border-stone-500/20 ml-30 pl-8 pr-0 pb-2 flex items-start text-stone-600 text-left text-pretty text-3xl font-light tracking-wide w-fit font-sans! uppercase">
+        {title}
+      </h3>
+      <TextBlock
+        classNames="h-fit  text-sm flex-inline flex-col  text-justify max-w-full tracking-tight font-light border-b-0 border-stone-400 ml-25 pl-13 pb-2      items-start pt-3 justify-center  text-stone-600 text-pretty text-[16px]"
+        content={lead}
+      />
+    </div>
+  );
+};
---
+++ app/components/sections/Project/Moment/CardLayout/CardLayout.tsx
@@ -0,0 +1,48 @@
+import { ReactNode, useEffect, useState } from "react";
+import "./CardLayout.css";
+export const MomentImage = ({ imageUrl }: { imageUrl: string }) => {
+  const effects =
+    "cursor-pointer duration-800 shadow-md hover:shadow-xl hover:scale-150 transform-3d perspective-1000 origin-center-right hover:md:scale-200 hover:md:left-0 border-white/80 hover:border-white/90 border-4 md:border-10 outline-olive-500 outline-3 hover:outline-olive-600/90";
+  return (
+    <div
+      className={`z-100 scale-110 md:scale-100 w-auto h-25 md:h-50 aspect-square shrink-0 rounded-full overflow-hidden bg-neutral-600 ${effects}`}>
+      <img
+        src={imageUrl}
+        alt={imageUrl}
+        className="brightness-75 hover:brightness-90 hover:saturate-120 opacity-90 hover:opacity-100 h-full aspect-video w-auto object-cover object-center scale-110 duration-1000"
+      />
+    </div>
+  );
+};
+
+interface Props {
+  title: string | ReactNode;
+  lead: string | ReactNode;
+  imageUrl: string;
+  position?: number;
+}
+
+export const CardLayout = ({ title, lead, imageUrl, position }: Props) => {
+  const [mLeft, setMLeft] = useState<string>();
+  useEffect(() => {
+    const positions = ["-ml-10", "ml-0 md:ml-25", "ml-10 md:ml-50"];
+    const marginLeft =
+      typeof position === "number" ? positions[position - 1] : positions[0];
+
+    !mLeft && position && setMLeft(marginLeft);
+  }, [position]);
+  if (!mLeft) return null;
+  return (
+    <div className={`${mLeft} flex mb-50 md:mb-35 mr-0 pl-0 ml-3 md:pl-12`}>
+      <MomentImage imageUrl={imageUrl} />
+      <div className="relative w-auto h-12.5 md:h-25 grow">
+        <h3 className="relative h-full pb-2 grow md:grow pl-16  mr-12 md:w-fit md:min-w-[65%] md:max-w-[75%] border-b-2 md:border-b-6 z-50 border-olive-500 flex items-end justify-end text-olive-600 text-right text-pretty text-2xl md:text-3xl font-semibold  ">
+          {title}
+          <div className="moment-lead absolute drop-shadow-2xl w-full text-pretty   top-full h-fit  md:max-h-[200%]   pb-2 overflow-hidden pl-4 md:pl-8 pt-3 font-sans font-normal text-[17px] md:text-lg tracking-tighter md:tracking-tight">
+            {lead}
+          </div>
+        </h3>
+      </div>
+    </div>
+  );
+};
