00be524fd4ac9125df387d4b8d421c87fa0cb698
what-anna-codes <annakarpiuk@zohomail.eu>
2026-07-29 03:00:08 +0200

    style: adjust margins & bgs

--- app/components/ui/Section/Section.tsx
+++ app/components/ui/Section/Section.tsx
@@ -14,8 +14,7 @@ export const Section = ({
   return (
     <div
       id={id}
-      className={`${bg || "bg-mauve-50"} my-8 flex flex-col justify-between shadow-2xl w-full grow flex-1 pb-0 pt-12 max-w-5xl
-      border-t border-b border-mauve-900/20 ${classNames}`}>
+      className={`${bg ?? "bg-mauve-100"} my-0 flex flex-col justify-between w-full grow flex-1 pb-0 pt-12 max-w-5xl! border-t border-b-40 border-white  ${classNames}`}>
       {children}
     </div>
   );
